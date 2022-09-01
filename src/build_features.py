cb69b09b85ce456cdb909e900740254248ecd933
Daniela Alvarez Zegarra <daniela.alvarez04@gmail.com>
2022-09-01 08:48:49 -0500

    removing constants

--- src/build_features.py
+++ src/build_features.py
@@ -1,12 +1,6 @@
 import pandas as pd
 import numpy as np
 
-from constants import COURSES
-from constants import LANGUAGES
-from constants import LOCATIONS
-from constants import UTM_MEDIUM
-from constants import UTM_SOURCE
-
 #PREPROCESSING FUNCTIONS
 
 def change_to_datetime(df,column):
@@ -30,97 +24,92 @@ def drop_test_rows(df):
     return df
 
 def remove_duplicates(df):
-    df = df.replace("Nan", np.nan)
-    df = df.sort_values("created_at")
+    print('shape before combining duplicates: ', df.shape)
+    df = df.replace("Nan", np.nan).copy()
+    df = df.sort_values("created_at").copy()
     df = df.groupby("email").first().reset_index()
+    print('shape after combining duplicates: ', df.shape)
     return df
     
 def clean_course(df,column):
-    for row in df:
-        df[column] = df[column].replace(COURSES[course], course)
-        print(df[column].value_counts())
+    df[column] = df[column].replace(['full-stack-ft', 'full_stack', 'full-stack,software-engineering',
+                                    'coding-introduction','outcomes'], 'full-stack')
+    df[column] = df[column].replace(['machine-learning', 'machine-learning-enginnering'], 
+                                    'machine-learning-engineering')
+    print(df[column].value_counts())
     return df
  
 def clean_location(df,column):
-    for location in LOCATIONS:
-        df[column] = df[column].replace(LOCATIONS[location], location)
-        print(df[column].value_counts())
+    df[column] = df[column].replace(['maracaibo'], 'maracaibo-venezuela')
+    df[column] = df[column].replace(['los-cortijos-caracas'], 'caracas-venezuela') 
+    print(df[column].value_counts())
     return df
   
     
 def clean_language(df,column):
-    for language in LANGUAGES:
-        df[column] = df[column].replace(LANGUAGES[language], language)
-        print(df[column].value_counts())
+    df[column] = df[column].replace('us', 'en')
+    print(df[column].value_counts())
     return df
     
 def clean_utm_medium(df,column):
-    for value in UTM_MEDIUM:
-        df[column] = df[column].replace(UTM_MEDIUM[value], value)
-        print(df[column].value_counts())
+    df[column] = df[column].replace(['schoolpage','coursereportschoolpage', 'schoolpage?utm_source=careerkarma',
+                                    'Blog','affiliate_email'], 'referral')
+    df[column] = df[column].replace(['23849575173750143','ppc','23849575173750143','Instagram_Feed','FB paid',
+                                    'Instagram_Stories','ML + AI','ML + AI v2 - 3rd time (Mar, lookalike NYC, NJ)'],
+                                    'cpc')
+    print(df[column].value_counts())
     return df      
     
-def clean_utm_source(df):
-    for value in UTM_SOURCE:
-        df[column] = df[column].replace(UTM_SOURCE[value], value)
-        print(df[column].value_counts())
+def clean_utm_source(df,column):
+    df[column] = df[column].replace('LInkedin', 'linkedin')
+    df[column] = df[column].replace('CourseReport', 'coursereport')
+    df[column] = df[column].replace(['landingjobs?utm_medium=machine-learning-engineering', 
+                                    'landingjobs?utm_medium=full-stack', 'landingjobs?utm_medium=RRSS'],
+                                    'landingjobs')
+    df[column] = df[column].replace('google_ads', 'google')
+    df[column] = df[column].replace(['Instagram_Feed', 'ig', 'Business Manager IG','23848557212380143',
+                                    '23849317251630143', 'Instagram_Stories'], 'instagram')
+    df[column] = df[column].replace(['23848655532190143','23850705303260143','23850859670310143','23850859616000143','Facebook','Facebook ads','Facebook_Mobile_Feed',
+                                    'facebook_instagram','fb','Facebook_Desktop_Feed','facebook_awareness',
+                                    'Facebook_Marketplace','Facebook_Stories','cpc','an'],
+                                    'facebook')
+    df[column] = df[column].replace(['PR?utm_medium=nota-prensa-1','python-es'], '4geeks' )
+    print(df[column].value_counts())
     return df
 
 def assign_lead_type(df,column1,column2):
-    
-    for row in df:
-        if df[column1] == 'request_more_info': 
-            df[column2] == 'SOFT'
-        elif df[column1] == 'website-lead': 
-            df[column2] == 'STRONG'
-        elif df[column1] == 'newsletter': 
-            df[column2] == 'DISCOVERY'
-        elif df[column1] == 'contact-us': 
-            df[column2] == 'SOFT'
-        elif df[column1] == 'utec-uruguay': 
-            df[column2] == 'STRONG'
-        elif df[column1] == 'jobboard-lead': 
-            df[column2] == 'STRONG'
-        elif df[column1] == 'hiring-partner': 
-            df[column2] == 'OTHER'
-        elif df[column1] == 'download_outcome': 
-            df[column2] == 'DISCOVERY'
-        elif df[column1] == 'website-lead,blacks-in-technology': 
-            df[column2] == 'STRONG'
-        elif df[column1] == 'request_downloadable': 
-            df[column2] == 'DISCOVERY'      
-        else:   
-            df[column2] == 'DISCOVERY'
-    
+    df.loc[df[column1] == 'request_more_info', column2] = 'SOFT'
+    df.loc[df[column1] == 'website-lead', column2] = 'STRONG'
+    df.loc[df[column1] == 'newsletter', column2] = 'DISCOVERY'
+    df.loc[df[column1] == 'contact-us', column2] = 'SOFT'
+    df.loc[df[column1] == 'utec-uruguay', column2] = 'STRONG'
+    df.loc[df[column1] == 'jobboard-lead', column2] = 'STRONG'
+    df.loc[df[column1] == 'hiring-partner', column2] = 'OTHER'
+    df.loc[df[column1] == 'download_outcome', column2] = 'DISCOVERY'
+    df.loc[df[column1] == 'website-lead,blacks-in-technology', column2] = 'STRONG'
+    df.loc[df[column1] == 'request_downloadable', column2] = 'DISCOVERY'
+    print(df[column2].value_counts())
     return df
 
 def assign_with_conditions(df):
-    df.loc[df['utm_source'] == 'landingjobs', 'utm_medium'] = 'referral',
-    
+    df.loc[df['utm_source'] == 'landingjobs', 'utm_medium'] = 'referral'
     df['utm_medium'] = np.where((df['utm_medium'] == 'Facebook_Mobile_Feed') & (df['has_gclid'] == '0') ,
                               'social', df['utm_medium'])
-
     df['utm_medium'] = np.where((df['utm_medium'] == 'Facebook_Mobile_Feed') & (df['has_gclid'] == '1') , 
                               'cpc', df['utm_medium'])
-
     df['utm_medium'] = np.where((df['utm_medium'] == '23849757712110143') & (df['has_gclid'] == '0') , 
                               'social', df['utm_medium'])
-
     df['utm_medium'] = np.where((df['utm_medium'] == '23849757712110143') & (df['has_gclid'] == '1') , 
                               'cpc', df['utm_medium'])
-
     df['utm_medium'] = np.where((df['utm_medium'] == 'Inmail') & (df['has_gclid'] == '0') , 
                               'social', df['utm_medium'])
-
     df['utm_medium'] = np.where((df['utm_medium'] == 'Facebook_Marketplace') & (df['has_gclid'] == '0') , 
                               'social', df['utm_medium'])
-
     df['utm_medium'] = np.where((df['utm_source'] == '4geeks') & (df['has_gclid'] == '0') , 
                               'social', df['utm_medium'])
-
     df['utm_medium'] = np.where((df['utm_medium'] == 'rrss') & (df['has_gclid'] == '0') , 
                               'social', df['utm_medium'])
-    
+    print('Assignation with conditions ok')
     return df
   
 #Visualization features
